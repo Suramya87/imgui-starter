f54924023501e35c3537466e0a1ae87e23d8962e
Suramya87 <146380955+Suramya87@users.noreply.github.com>
2026-01-20 14:47:55 -0800

    dont ever have the build folder over there

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/suram/Desktop/Cmpm_123/Cmpm123/imgui-starter
-# Build directory: C:/Users/suram/Desktop/Cmpm_123/Cmpm123/imgui-starter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
